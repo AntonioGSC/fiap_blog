21cc3ca8293c029f8369eeb28e7406efc8fb0277
Antonio Carvalho <antoniogabrielagsc@gmail.com>
2024-02-06 23:17:47 -0300

    feat: blog structure

---
+++ src/pages/Home.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react';
+import Layout from '../components/Layout'
+import { client } from '../lib/createClient';
+
+
+const Home = () => {
+    const [categories, setCategories] = useState([]);
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        client
+            .getEntries({
+                content_type: 'blogCategory',
+                order: 'sys.createdAt'
+            })
+            .then((entries) => {
+                setCategories(entries.items);
+            });
+
+        client
+            .getEntries({
+                content_type: 'blogPost',
+                limit: 5,
+                order: '-sys.createdAt'
+            })
+            .then((entries) => {
+                setPosts(entries.items);
+                console.log(entries)
+            });
+    }, []);
+
+    return (
+        <>
+            <Layout>
+                <div className="container">
+                    <div className="row">
+                        <main className="col-md-8">
+                            <h1 className="my-3">Últimos posts</h1>
+
+                            {posts.map((post) => {
+                                return (
+                                    <div className="card mb-3" key={post.sys.id}>
+                                        <div className="card-body">
+                                            <h5 className="card-title">{post.fields.postTitle}</h5>
+                                            <p className="card-text">{post.fields.postDescription}</p>
+                                        </div>
+                                    </div>
+                                );
+                            })}
+
+                            <a href="#" className='btn btn-primary'>Ver todos os posts</a>
+                        </main>
+
+                        <aside className="col-md-4">
+                            <h2>Categorias</h2>
+                            <ul>
+                                {categories.map((category) => {
+                                    return (
+                                        <li key={category.sys.id}>{category.fields.categoryTitle}</li>
+                                    );
+                                })}
+                            </ul>
+                        </aside>
+                    </div>
+                </div>
+            </Layout>
+        </>
+    )
+}
+
+export default Home;
