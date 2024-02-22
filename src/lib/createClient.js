0af9e513b4eb7f21f04d41692bf2000d0cb774a6
Antonio Carvalho <antoniogabrielagsc@gmail.com>
2024-02-21 21:37:35 -0300

    fix: using .env file

--- src/lib/createClient.js
+++ src/lib/createClient.js
@@ -1,12 +1,5 @@
 import * as contentful from 'contentful';
 
-// import { spaceId, accessToken } from '../constants';
-
-// export const client = contentful.createClient({
-//     space: spaceId,
-//     accessToken
-// });
-
 export const client = contentful.createClient({
     space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
     accessToken: import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN,
